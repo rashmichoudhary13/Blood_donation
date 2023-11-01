f6fe83f93c9657c2ac4a961d516cbb7ab51a7d35
rashmichoudhary13 <rashmichoudhary019@gmail.com>
2023-11-02 00:46:14 +0530

    logo added

--- user/include/header.php
+++ user/include/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
 	<head>
-		<title>Donate The Blood</title>
+		<title>WeCare: A Donatin website</title>
 		<meta charset="utf-8">
 
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
