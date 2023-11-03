7423726103cd53c0760dbf0c6dcedd50a17f0634
rashmichoudhary13 <rashmichoudhary019@gmail.com>
2023-11-03 11:10:08 +0530

    removed img from the logo

--- include/navigation.php
+++ include/navigation.php
@@ -1,7 +1,7 @@
 <body>
 <!-- Navigation starts -->
 <nav id="mainNav" class="navbar fixed-top navbar-default navbar-expand-lg navbar-light bg-light own-nav">
-  <a class="navbar-brand" href="./index.php"><img src="../img/logo.png" class="logo"></a>
+  <a class="navbar-brand" href="./index.php">WeDonate</a>
   
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
